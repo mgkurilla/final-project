93319e496e7eb26e43854d5c30109f608d33fa05
Michelle Kurilla <71084282+mgkurilla@users.noreply.github.com>
2020-11-24 11:02:52 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -16,17 +16,17 @@ library(viridis)
 
 # Define UI for application that draws a histogram
 
-csi_index <- 
+csi_index <-
   read_excel("CSI_1953_2014.xls")
 
-  # use view(csi_index) to ensure file displays properly
+# use view(csi_index) to ensure file displays properly
 
 csi_index_by_year <- csi_index %>%
- select(caseId, CSI) %>%
- mutate(caseId = str_sub(caseId, 1, 4)) %>%
- group_by(caseId, CSI) %>%
- tally() %>%
- rename(totalCSI = n)
+  select(caseId, CSI) %>%
+  mutate(caseId = str_sub(caseId, 1, 4)) %>%
+  group_by(caseId, CSI) %>%
+  tally() %>%
+  rename(totalCSI = n)
 
 csi_index_by_decade <- csi_index %>%
   select(caseId, CSI) %>%
@@ -39,108 +39,157 @@ csi_index_by_decade <- csi_index %>%
 
 
 ui <- fluidPage(
-  navbarPage(theme = shinytheme("yeti"),
-             "Milestone 6",
-    tabPanel("CSI Index by Newspaper",
-             titlePanel("CSI Index by Newspaper"), 
-                         selectInput(
-                             inputId = "var",
-                             label = "Newspaper",
-                             choices = c("LA Times" = "laScore", "Chicago Tribune" = "chScore", 
-                               "Washington Post" = "washScore", 
-                               "New York Times" = "nyScore")),
-                         mainPanel(plotOutput("line_plot"))),
-    
-    tabPanel("CSI Index by Year",
-               titlePanel("CSI Index Frequency by Year"), 
-        selectInput(
-          inputId = "csi_index_by_year",
-          label = "Year",
-          choices = c("1953", "1954", "1955", "1956", 
-                      "1957", "1958", "1959", "1960", 
-                      "1961", "1962", "1963", "1964", 
-                      "1965", "1966", "1967", "1968", 
-                      "1969", "1970", "1971", "1972", 
-                      "1973", "1974", "1975", "1976", 
-                      "1977", "1978", "1979", "1980", 
-                      "1981", "1982", "1983", "1984", 
-                      "1985", "1986", "1987", "1988", 
-                      "1989", "1990", "1991", "1992", 
-                      "1993", "1994", "1995", "1996", 
-                      "1997", "1998", "1999", "2000", 
-                      "2001", "2002", "2003", "2004", 
-                      "2005", "2006", "2007", "2008", 
-                      "2009", "2010", "2011", "2012", 
-                      "2013", "2014")),
-      mainPanel(plotOutput("csi_hist_year"))),
-  
-  tabPanel("CSI Index by Decade",  
-    titlePanel("CSI Index Frequency by Decade"), 
+  navbarPage(
+    theme = shinytheme("flatly"),
+    "Case Salience and Chief Justices",
+    tabPanel("Home",
+             titlePanel("Welcome"),
+             h3("Elevator Pitch")),
+    tabPanel(
+      "CSI Index Breakdown",
+      titlePanel("CSI Index by Newspaper"),
+      selectInput(
+        inputId = "var",
+        label = "Newspaper",
+        choices = c(
+          "LA Times" = "laScore",
+          "Chicago Tribune" = "chScore",
+          "Washington Post" = "washScore",
+          "New York Times" = "nyScore")),
+      mainPanel(plotOutput("line_plot")),
+    titlePanel("CSI Index Frequency by Year"),
+    selectInput(
+      inputId = "csi_index_by_year",
+      label = "Year",
+      choices = c(
+        "1953",
+        "1954",
+        "1955",
+        "1956",
+        "1957",
+        "1958",
+        "1959",
+        "1960",
+        "1961",
+        "1962",
+        "1963",
+        "1964",
+        "1965",
+        "1966",
+        "1967",
+        "1968",
+        "1969",
+        "1970",
+        "1971",
+        "1972",
+        "1973",
+        "1974",
+        "1975",
+        "1976",
+        "1977",
+        "1978",
+        "1979",
+        "1980",
+        "1981",
+        "1982",
+        "1983",
+        "1984",
+        "1985",
+        "1986",
+        "1987",
+        "1988",
+        "1989",
+        "1990",
+        "1991",
+        "1992",
+        "1993",
+        "1994",
+        "1995",
+        "1996",
+        "1997",
+        "1998",
+        "1999",
+        "2000",
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014"
+      )
+    ),
+    mainPanel(plotOutput("csi_hist_year")),
+    titlePanel("CSI Index Frequency by Decade"),
     selectInput(
       inputId = "csi_index_by_decade",
       label = "Decade",
-      choices = c("195", "196", "197", "198", "199", 
+      choices = c("195", "196", "197", "198", "199",
                   "200", "201")),
     mainPanel(plotOutput("csi_hist_decade"))),
-
-   # decided to use yeti theme because it looked the best
-   # remember to name newspapers as choices 
-   # be mindful of the names assigned
-   # note that tabPanel() creates a new tab
-   
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("I wanted to show how case salience may vary by newspaper.")),
-    tabPanel("About", 
-             titlePanel("About"),
-             h3("Project Background and Motivations"),
-             p("Hello, I am working on this project to 
+  # decided to use yeti theme because it looked the best
+  # remember to name newspapers as choices
+  # be mindful of the names assigned
+  # note that tabPanel() creates a new tab
+  
+  tabPanel("About",
+    titlePanel("About"),
+    p("I wanted to show how case salience may vary by newspaper."),
+    h3("Project Background and Motivations"),
+    p(
+      "Hello, I am working on this project to
                determine Supreme Court majority opinion assignments by the Chief
-               Justice as they relate to case salience."),
-             h3("About Me"),
-             p("My name is Michelle Kurilla and I study English and Government.")),
-    tabPanel("Methodology", 
-             titlePanel("Methodology"),
-             h3("Methods and Data Wrangling"),
-             p("My project includes data from the Harvard Dataverse and the 
-               Supreme Court database."))))
+               Justice as they relate to case salience."
+    ),
+    h3("About Me"),
+    p("My name is Michelle Kurilla and I study English and Government."),
+    h3("Methods and Data Wrangling"),
+    p("My project includes data from the Harvard Dataverse and the
+      Supreme Court database."))))
 
-    server <- function(input, output) {
-        output$line_plot <- renderPlot({
-            data <- csi_index %>% select(.data[[input$var]])
-            ggplot(data, mapping = aes(x = .data[[input$var]])) + 
-            geom_bar() + 
-            labs(title = "Case Salience Index by Newspaper",
-                 x = "CSI", 
-                y = "Total Count of CSI")
-        })
-        
-        output$csi_hist_year <- renderPlot({
-          data <- csi_index_by_year %>% 
-            filter(caseId == input$csi_index_by_year)
-          ggplot(data, mapping = aes(x = totalCSI, fill = factor(CSI))) + 
-                   geom_bar() + 
-            theme_bw() + 
-            labs(title = "CSI by Year", 
-                 x = "CSI", 
-                 y = "Total Count of CSI")
-      
-        })
-        
-        output$csi_hist_decade <- renderPlot({
-          data <- csi_index_by_decade %>% 
-            filter(decade == input$csi_index_by_decade)
-          ggplot(data, mapping = aes(x = totalCSI_decade, fill = factor(CSI))) + 
-            geom_bar() + 
-            theme_bw() + 
-            labs(title = "CSI by Decade", 
-                 x = "CSI", 
-                 y = "Total Count of CSI")
-          
-    })
-        
-    }
+server <- function(input, output) {
+  output$line_plot <- renderPlot({
+    data <- csi_index %>% select(.data[[input$var]])
+    ggplot(data, mapping = aes(x = .data[[input$var]])) +
+      geom_bar() +
+      labs(title = "Case Salience Index by Newspaper",
+           x = "CSI",
+           y = "Total Count of CSI")
+  })
+  
+  output$csi_hist_year <- renderPlot({
+    data <- csi_index_by_year %>%
+      filter(caseId == input$csi_index_by_year)
+    ggplot(data, mapping = aes(x = totalCSI, fill = factor(CSI))) +
+      geom_bar() +
+      theme_bw() +
+      labs(title = "CSI by Year",
+           x = "CSI",
+           y = "Total Count of CSI")
     
+  })
+  
+  output$csi_hist_decade <- renderPlot({
+    data <- csi_index_by_decade %>%
+      filter(decade == input$csi_index_by_decade)
+    ggplot(data, mapping = aes(x = totalCSI_decade, fill = factor(CSI))) +
+      geom_bar() +
+      theme_bw() +
+      labs(title = "CSI by Decade",
+           x = "CSI",
+           y = "Total Count of CSI")
+    
+  })
+  
+}
+
     # Run the application -----
 
 shinyApp(ui, server)
